22e69c002e820dfafe3ae00b56f15d816f081117
Timo Bryant <t.bryant@insiders-technologies.de>
2020-12-09 17:32:17 +0100

    implement delete-tags

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use reqwest::{IntoUrl, Url, Client};
 use serde::Deserialize;
+use serde::de::DeserializeOwned;
+use reqwest::header::HeaderValue;
+use log::info;
+
 
 pub type Result<T, E = std::boxed::Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
@@ -14,19 +18,59 @@ struct CatalogResponse {
     repositories: Vec<String>
 }
 
+#[derive(Deserialize)]
+struct TagsListResponse {
+    name: String,
+    tags: Vec<String>
+}
+
 impl DockerRegistry {
     pub fn new<U: IntoUrl>(url: U) -> Result<Self> {
         Ok(DockerRegistry { url: url.into_url()?, client: Default::default() })
     }
 
     pub async fn list_repos(&self) -> Result<Vec<String>> {
-        let target = self.url.join("/v2/_catalog")?;
-        let response: CatalogResponse = self.client.get(target)
+        let response: CatalogResponse = self.http_get("/v2/_catalog").await?;
+        Ok(response.repositories)
+    }
+
+    pub async fn list_tag_per_repo(&self, repo_name: &str) -> Result<Vec<String>> {
+        let path = format!("/v2/{}/tags/list", repo_name);
+        let response: TagsListResponse = self.http_get(path.as_str()).await?;
+        Ok(response.tags)
+    }
+
+    pub async fn delete_tag(&self, repo_name: &str, tag: &str) -> Result<()> {
+        let digest = self.retrieve_digest(repo_name, tag).await?.expect("no such tag");
+        //info!("Digest = {}", digest);
+        //jlet splitted: Vec<&str> =  digest.splitn(2, ":").collect();
+        //let digest = splitted[1];
+        let target = self.url.join(format!("/v2/{}/manifests/{}", repo_name, digest).as_str())?;
+        self.client.delete(target)
+            .send()
+            .await?;
+        Ok(())
+    }
+
+    async fn retrieve_digest(&self, repo_name: &str, tag: &str) -> Result<Option<String>> {
+        let target = self.url.join(format!("/v2/{}/manifests/{}", repo_name, tag).as_str())?;
+        let response = self.client.head(target)
+            .header("Accept","application/vnd.docker.distribution.manifest.v2+json")
+            .send().await?;
+        let digest = response.headers().get("Docker-Content-Digest");
+        match digest {
+            None => Ok(None),
+            Some(header_value) => Ok(Some(String::from(header_value.to_str()?)))
+        }
+    }
+
+    async fn http_get<T: DeserializeOwned>(&self, path: &str) -> Result<T> {
+        let target = self.url.join(path)?;
+        Ok(self.client.get(target)
             .send()
             .await?
             .json()
-            .await?;
-        Ok(response.repositories)
+            .await?)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use structopt::StructOpt;
 use dr::DockerRegistry;
+use log::info;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "docker registry client")]
 enum Args {
     ListRepos {
         url: String
+    },
+    ListTags {
+        url: String,
+        repo: String
+    },
+    DeleteTags {
+        url: String,
+        repo: String,
+        tags: Vec<String>
     }
 }
 
@@ -14,7 +24,9 @@ async fn main() -> dr::Result<()> {
     pretty_env_logger::init_timed();
     let args: Args = Args::from_args();
     match args {
-        Args::ListRepos { url } => list_repos(url).await?
+        Args::ListRepos { url } => list_repos(url).await?,
+        Args::ListTags { url, repo} => list_tags(url, repo).await?,
+        Args::DeleteTags { url, repo, tags } => delete_tags(url, repo, tags).await?
     };
     Ok(())
 }
@@ -27,3 +39,20 @@ async fn list_repos(url: String) -> dr::Result<()> {
     Ok(())
 }
 
+
+async fn list_tags(url: String, repo: String) -> dr::Result<()> {
+    let registry = DockerRegistry::new(&url)?;
+    for tag in registry.list_tag_per_repo(repo.as_str()).await?.into_iter() {
+        println!("{}", tag)
+    }
+    Ok(())
+}
+
+async fn delete_tags(url: String, repo: String, tags: Vec<String>) -> dr::Result<()> {
+    let registry = DockerRegistry::new(&url)?;
+    for tag in tags.into_iter() {
+        info!("Deleting: {}", tag);
+        registry.delete_tag(repo.as_str(), tag.as_str()).await?;
+    }
+    Ok(())
+}
