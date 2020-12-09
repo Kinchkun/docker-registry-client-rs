75699c6147146efd104df5599edc8a0252b2aa2e
Timo Bryant <t.bryant@insiders-technologies.de>
2020-12-09 15:34:20 +0100

    implement list-repos

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+use reqwest::{IntoUrl, Url, Client};
+use serde::Deserialize;
+
+pub type Result<T, E = std::boxed::Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+pub struct DockerRegistry {
+    url: Url,
+    client: Client
+}
 
 
-struct DockerRegistry {}
+#[derive(Deserialize)]
+struct CatalogResponse {
+    repositories: Vec<String>
+}
+
+impl DockerRegistry {
+    pub fn new<U: IntoUrl>(url: U) -> Result<Self> {
+        Ok(DockerRegistry { url: url.into_url()?, client: Default::default() })
+    }
+
+    pub async fn list_repos(&self) -> Result<Vec<String>> {
+        let target = self.url.join("/v2/_catalog")?;
+        let response: CatalogResponse = self.client.get(target)
+            .send()
+            .await?
+            .json()
+            .await?;
+        Ok(response.repositories)
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use dr::DockerRegistry;
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "docker registry client")]
@@ -9,9 +10,20 @@ enum Args {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> dr::Result<()> {
     pretty_env_logger::init_timed();
     let args: Args = Args::from_args();
-    println!("{:#?}", args)
+    match args {
+        Args::ListRepos { url } => list_repos(url).await?
+    };
+    Ok(())
+}
+
+async fn list_repos(url: String) -> dr::Result<()> {
+    let registry = DockerRegistry::new(&url)?;
+    for repo in registry.list_repos().await?.into_iter() {
+        println!("{}", repo)
+    }
+    Ok(())
 }
 
